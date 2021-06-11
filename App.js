69c24011bfbee6f4e6302515e9deec05baeed7b3
TANAV <tanavsaraf@gmail.com>
2021-06-11 19:59:04 +0530

    CHUNKS

--- App.js
+++ App.js
@@ -1,29 +1,47 @@
-
-import * as React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import {Header} from 'react-native-elements'
-export default class App extends React.Component{
-    constructor()
-    {
-        super()
-        this.state={
-            text:'',
-            displayText:''
-        }
-    }
-    render()
-    {
-        return(<View>
-            <Header centerComponent={{text:'MONKEY CHUNKY'}}></Header>
-            <TextInput style={{borderWidth:20,marginTop:30}} onChangeText={(data)=>{
-                    this.setState({text:data})
-            }}/>
-            <TouchableOpacity onPress={()=>{
-                this.setState({displayText:this.state.text})
-            }}><Text>GO</Text></TouchableOpacity>
-            <Text>{this.state.displayText}</Text>
-        </View>)
-    }
+import * as React from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from "react-native";
+/*import { Header } from "react-native-elements";
+<Header centerComponent={{ text: "MONKEY CHUNKY" }}></Header>*/
+import db from "./localDb";
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: "",
+      chunks: [],
+    };
+  }
+  render() {
+    return (
+      <View>
+        
+        <TextInput
+          style={{ borderWidth: 20, marginTop: 30 }}
+          onChangeText={(data) => {
+            this.setState({ text: data });
+          }}
+        />
+        <TouchableOpacity
+          onPress={() => {
+            this.setState({ chunks: db[this.state.text].chunks });
+          }}
+        >
+          <Text>GO</Text>
+        </TouchableOpacity>
+        <View>
+          {this.state.chunks.map((item) => (
+            <TouchableOpacity>
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+    );
+  }
 }
-
-
